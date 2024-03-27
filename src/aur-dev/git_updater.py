3ff41d493ab6526f7f7a7a6f5ddae1a9ae7d42f3
Felipe <f.alfonso@res-ear.ch>
2024-03-27 06:23:55 -0300

    Updates src code

--- src/aur-dev/git_updater.py
+++ src/aur-dev/git_updater.py
@@ -5,7 +5,7 @@ def welcome():
     print("\033[1;32mThis software was developed by Computer Science Engineer Felipe Alfonso González - Github: github.com/felipealfonsog - Under the BSD 3-clause license.\033[0m")
     print("\033[1;32mDeveloped from Chile with love.\033[0m")
     print("\033[1;32m----------------------------------------------------\033[0m")
-    print("\033[1;32m\033[1mThis software will update all GitHub repositories within the current directory or its subdirectories.\033[0m")
+    print("\033[1;32m\033[1mEffortlessly Automate Git Repository Updates, Including Committing and Pulling, Across Directory Structures.\033[0m")
 
 def update_github_repositories(main_directory, include_aur):
     print("\nUpdating GitHub repositories...\n")
