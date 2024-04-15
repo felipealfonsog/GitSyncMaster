4914a6916ce125205e68f35d86470f5aae41d48c
Fellipe Alfonso González <f.alfonso@res-ear.ch>
2024-04-15 18:58:27 -0400

    Updates

--- installer_AppleM2.sh
+++ installer_AppleM2.sh
@@ -62,27 +62,6 @@ check_homebrew_installation_macOS() {
     fi
 }
 
-check_homebrew_installation_linux() {
-    if ! command -v brew &> /dev/null; then
-        echo "Homebrew/Linuxbrew is not installed on Linux. Do you want to install it?"
-        select yn in "Yes" "No"; do
-            case $yn in
-                Yes)
-                    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-                    break
-                    ;;
-                No)
-                    echo "Exiting program."
-                    exit 0
-                    ;;
-                *)
-                    echo "Invalid option. Please choose a valid option."
-                    ;;
-            esac
-        done
-    fi
-}
-
 install_dependencies_macOS() {
     if ! command -v python3 &> /dev/null; then
         echo "Python3 is not installed on macOS. Do you want to install it?"
@@ -104,83 +83,21 @@ install_dependencies_macOS() {
     fi
 }
 
-install_dependencies_linux() {
-    if ! command -v python3 &> /dev/null; then
-        echo "Python3 is not installed on Linux. Do you want to install it?"
-        select yn in "Yes, using Homebrew" "Yes, using package manager" "No"; do
-            case $yn in
-                "Yes, using Homebrew")
-                    brew install python
-                    break
-                    ;;
-                "Yes, using package manager")
-                    if [[ -f /etc/arch-release ]]; then
-                        sudo pacman -S python
-                    elif [[ -f /etc/debian_version ]]; then
-                        sudo apt-get update && sudo apt-get install python3
-                    else
-                        echo "Unsupported Linux distribution. Please install Python3 manually, read documentation according to your distro, and re-run the installer."
-                        exit 1
-                    fi
-                    break
-                    ;;
-                No)
-                    echo "Exiting program."
-                    exit 0
-                    ;;
-                *)
-                    echo "Invalid option. Please choose a valid option."
-                    ;;
-            esac
-        done
-    fi
-}
-
 download_source_code() {
-    if [[ $(uname) == "Darwin" ]]; then
-        source_file_url="https://raw.githubusercontent.com/felipealfonsog/GitSyncMaster/main/src/macos-linux-dev/git_update_macos.py"
-        source_file_name="git_update_macos.py"
-    elif [[ $(uname) == "Linux" ]]; then
-        source_file_url="https://raw.githubusercontent.com/felipealfonsog/GitSyncMaster/main/src/macos-linux-dev/git_update_linux.py"
-        source_file_name="git_update_linux.py"
-    else
-        echo "Unsupported operating system. Please install manually, read documentation, and re-run the installer."
-        exit 1
-    fi
+    source_file_url="https://raw.githubusercontent.com/felipealfonsog/GitSyncMaster/main/src/macos-linux-dev/git_update_macosm-x.py"
+    source_file_name="git_update_macos.py"
 
     curl -o "$source_file_name" "$source_file_url"
 }
 
 move_exec_file() {
-    # Move the executable file to appropriate directory based on the OS
-    if [[ $(uname) == "Darwin" ]]; then
-        sudo cp "$source_file_name" /usr/local/bin/gitsync
-        sudo chmod +x "/usr/local/bin/gitsync"
-    else
-        sudo cp "$source_file_name" /usr/bin/gitsync
-        sudo chmod +x /usr/bin/gitsync
-    fi
+    sudo cp "$source_file_name" /usr/local/bin/gitsync
+    sudo chmod +x "/usr/local/bin/gitsync"
 }
 
 configure_path() {
-    if [[ $(uname) == "Darwin" ]]; then
-        echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.zshrc
-        source ~/.zshrc
-    else
-        if [[ -f ~/.zshrc ]]; then
-            echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.zshrc
-            source ~/.zshrc
-        elif [[ -f ~/.bashrc ]]; then
-            echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
-            source ~/.bashrc
-        elif [[ -f ~/.bash_profile ]]; then
-            echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile
-            source ~/.bash_profile
-        else
-            echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.profile
-            source ~/.profile
-        fi
-    fi
+    echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.zshrc
+    source ~/.zshrc
 }
 
 cleanup() {
@@ -198,14 +115,8 @@ cleanup() {
 main() {
     welcome
     check_execute_permission
-
-    if [[ $(uname) == "Darwin" ]]; then
-        check_homebrew_installation_macOS
-        install_dependencies_macOS
-    elif [[ $(uname) == "Linux" ]]; then
-        check_homebrew_installation_linux
-        install_dependencies_linux
-    fi
+    check_homebrew_installation_macOS
+    install_dependencies_macOS
 
     download_source_code
     move_exec_file
